886e5989893d98aa467b3d369e2ddae9c865cf73
Sujal Kalra <114348922+sujalkalra@users.noreply.github.com>
2024-11-16 22:54:01 +0530

    Update articles.js

--- src/pages/articles.js
+++ src/pages/articles.js
@@ -7,11 +7,11 @@ import { motion, useMotionValue } from 'framer-motion';
 import TransitionEffect from '@/components/TransitionEffect';
 
 // Importing images
-import article1 from '../../public/images/articles/pagination-component-in-reactjs.jpg';
-import article2 from '../../public/images/articles/create-loading-screen-in-react-js.jpg';
-import article3 from '../../public/images/articles/create-modal-component-in-react-using-react-portals.png';
-import article4 from '../../public/images/articles/form-validation-in-reactjs-using-custom-react-hook.png';
-import article5 from '../../public/images/articles/smooth-scrolling-in-reactjs.png';
+import article1 from 'public/images/articles/pagination-component-in-reactjs.jpg';
+import article2 from 'public/images/articles/create-loading-screen-in-react-js.jpg';
+import article3 from 'public/images/articles/create-modal-component-in-react-using-react-portals.png';
+import article4 from 'public/images/articles/form-validation-in-reactjs-using-custom-react-hook.png';
+import article5 from 'public/images/articles/smooth-scrolling-in-reactjs.png';
 
 const FramerImage = motion(Image);
 
