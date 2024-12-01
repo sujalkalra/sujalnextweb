911aefdb07e198d686be4f30f8acbe84c7132761
Sujal Kalra <114348922+sujalkalra@users.noreply.github.com>
2024-12-02 01:26:37 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -41,7 +41,7 @@ export default function Home() {
                 download={true}>Resume<LinkArrow className={"w-6 ml-1 "}/></Link>
 
 
-                <Link href="mailto:####@gmail.com" target={'_blank'}
+                <Link href="mailto:kalrasujal322@gmail.com" target={'_blank'}
                 className='ml-4 text-lg font-medium capitalize text-dark underline dark:text-light md:text-base'>Contact</Link>
               </div>
             </div>
