b982c1bb96359f55cd2c91745468f966eca236f9
Sujal Kalra <114348922+sujalkalra@users.noreply.github.com>
2024-12-01 11:53:42 +0530

    Update NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -70,11 +70,11 @@ const NavBar = () => {
                     <CustomLink href="/articles" title='Articles' className='ml-4' />
                 </nav>
                 <nav className='flex items-center justify-center flex-wrap'>
-                    <motion.a href='###' target={'_blank'} whileHover={{ y: -2 }} whileTap={{ scale: 0.9 }} className='w-6 mr-3'><LeetCodeIcon /></motion.a >
-                    <motion.a href='###' target={'_blank'} whileHover={{ y: -2 }} whileTap={{ scale: 0.9 }} className='w-6 mx-3'><GithubIcon /></motion.a >
-                    <motion.a href='###' target={'_blank'} whileHover={{ y: -2 }} whileTap={{ scale: 0.9 }} className='w-6 mx-3'><LinkedInIcon /></motion.a >
-                    <motion.a href='###' target={'_blank'} whileHover={{ y: -2 }} whileTap={{ scale: 0.9 }} className='w-6 mx-3 bg-light rounded-full'><PinterestIcon /></motion.a >
-                    <motion.a href='###' target={'_blank'} whileHover={{ y: -2 }} whileTap={{ scale: 0.9 }} className='w-6 ml-3'><DribbbleIcon /></motion.a >
+                    <motion.a href='https://leetcode.com/u/SUJAL_KALRA/' target={'_blank'} whileHover={{ y: -2 }} whileTap={{ scale: 0.9 }} className='w-6 mr-3'><LeetCodeIcon /></motion.a >
+                    <motion.a href='https://github.com/sujalkalra' target={'_blank'} whileHover={{ y: -2 }} whileTap={{ scale: 0.9 }} className='w-6 mx-3'><GithubIcon /></motion.a >
+                    <motion.a href='https://www.linkedin.com/in/sujal-kalra-660190252/' target={'_blank'} whileHover={{ y: -2 }} whileTap={{ scale: 0.9 }} className='w-6 mx-3'><LinkedInIcon /></motion.a >
+                    <motion.a href='https://in.pinterest.com/kalrasujal322/' target={'_blank'} whileHover={{ y: -2 }} whileTap={{ scale: 0.9 }} className='w-6 mx-3 bg-light rounded-full'><PinterestIcon /></motion.a >
+                    <motion.a href='https://www.instagram.com/compilersuji/' target={'_blank'} whileHover={{ y: -2 }} whileTap={{ scale: 0.9 }} className='w-6 ml-3'><DribbbleIcon /></motion.a >
 
                     <button onClick={() => setMode(mode === "light" ? "dark" : "light")}
                         className={`ml-3 flex items-center justify-center rounded-full p-1
