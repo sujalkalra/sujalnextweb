cbdae285ecd0177903be967ae5b5726dbee056b3
Sujal Kalra <114348922+sujalkalra@users.noreply.github.com>
2025-02-06 21:31:16 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,13 +7,13 @@ import Link from 'next/link';
 	  <footer className='w-full border-t-2 border-solid border-dark
       font-medium text-lg dark:text-light dark:border-light sm:text-base'>
         <Layout className='py-8 flex items-center justify-between lg:flex-col lg:py-6'>
-            <span>{new Date().getFullYear()}&copy; All Rights Reserved.</span>
+            <span>{new Date().getFullYear()} &copy; All Rights Reserved.</span>
             <div className="flex items-center lg:py-2">
                 built with<span className='text-primary dark:text-primaryDark text-2xl px-1'>&#9825;</span>by&nbsp;
                 <Link href='https://github.com/sujalkalra' className='underline underline-offset-2'>SujalKalra</Link>
             </div>
             
-            <Link href='https://compilersuji-guests.vercel.app/' target={'_blank'} className='underline underline-offset-2'>Say Hello</Link>
+            <Link href='https://sujikiguestbook2.vercel.app/' target={'_blank'} className='underline underline-offset-2'>Say Hello</Link>
         </Layout>
       </footer>
 	);
